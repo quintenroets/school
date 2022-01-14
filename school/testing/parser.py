dbfe67cb4e23c6823c7058f77826d4fa5561fab4
Quinten <quinten.roets@gmail.com>
2022-01-15 00:45:11 +0100

    adapt to new gui

--- school/testing/parser.py
+++ school/testing/parser.py
@@ -1,7 +1,6 @@
 import cli
+import gui
 
-from libs.cli import Cli
-from libs.gui import Gui
 from school.path import Path
 
 
@@ -9,11 +8,11 @@ class Parser:
     @staticmethod
     def start():
         sorts = ["content_toc", "zoom"]
-        sort = Gui.ask("Choose content sort", sorts)
+        sort = gui.ask("Choose content sort", sorts)
         if sort:
             folder = Path.assets / sort
             filenames = {f.stem: f for f in folder.glob("*.txt")}
-            filename: Path = Gui.ask("Choose course", filenames)
+            filename: Path = gui.ask("Choose course", filenames)
 
             if filename:
                 with filename.with_suffix(".yaml") as tempfile:
