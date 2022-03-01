143c219b52669fad38dd0a0e55db04428e92a022
Quinten <quinten.roets@gmail.com>
2022-03-02 00:36:00 +0100

    update content again

--- school/utils/constants.py
+++ school/utils/constants.py
@@ -4,6 +4,6 @@ email = os.environ["email"].replace("gmail.com", "ugent.be")
 pw = os.environ["pw"]
 root_url = "https://ufora.ugent.be/"
 
-update_content = False
+update_content = True
 overwrite_downloads = True
 one_course_nr = None
